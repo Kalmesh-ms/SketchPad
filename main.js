69ec4b9a89f0902c645da867f292439d33f2c4d5
Kalmesh-ms <141908865+Kalmesh-ms@users.noreply.github.com>
2023-09-05 19:30:39 +0530

    Add changeColor @grid ifmouse(over && down)

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_COLOR = 'black';
 
 let currGridSize = DEFAULT_GRID_SIZE;
+let currColor = DEFAULT_COLOR;
 
 let xGrid =[];
 let yGrid =[];
 
 const grid = document.querySelector('#grid');
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown =true);
+document.body.onmouseup = () => (mouseDown =false);
 
 
 function setGridSize (currGridSize){
@@ -13,10 +19,11 @@ function setGridSize (currGridSize){
         let xDiv = document.createElement('div');
         xDiv.classList.add('rows')
         xGrid[i]=xDiv;
-        // console.log(xGrid[i])
         grid.appendChild(xGrid[i]);
         for ( let j=0 ; j<currGridSize ; j++){
             let newDiv = document.createElement('div');
+            newDiv.addEventListener('mouseover', changeColor);
+            newDiv.addEventListener('mousedown', changeColor);
             newDiv.classList.add('px');
             yGrid[j] = newDiv;
             xGrid[i].appendChild(yGrid[j]);
@@ -24,6 +31,17 @@ function setGridSize (currGridSize){
 }
 }
 
+function setColor(newColor){
+    currColor = newColor;
+}
+
+function changeColor (e){
+    if(e.type === 'mouseover' && !mouseDown)return;
+    e.target.classList.add('colored');
+    if (currColor === 'black'){
+        e.target.style.backgroundColor = 'black';
+    }
+}
 
 
 
@@ -35,6 +53,5 @@ function setGridSize (currGridSize){
 
 
 
-
-setGridSize(7);
+setGridSize(DEFAULT_GRID_SIZE);
 
